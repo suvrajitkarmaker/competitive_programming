455916e427cbbe8f4de004a091eac5555ece466f
Suvrajit karmaker <38050162+suvrajitkarmaker@users.noreply.github.com>
2018-10-23 20:45:56 +0600

    Update 108.cpp

--- uva/108.cpp
+++ uva/108.cpp
@@ -1,43 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 int main()
 {
 
     int n;
     while(scanf("%d",&n)!=EOF)
     {
-        int a[n][n];
-        for(int i=0; i<n; i++)
-            for(int j=0; j<n; j++)
-            {
-                scanf("%d",&a[i][j]);
-                if(i>0)
-                    a[i][j]+=a[i-1][j];
-                if(j>0)
-                    a[i][j]+=a[i][j-1];
-                if(i>0 && j>0)
-                    a[i][j]-=a[i-1][j-1];
-            }
-        int sm,mx=-99999999;
-        for(int i=0; i<n; i++)
+        int a[n][n],sum[n+1],mx=-99999999;
+        for(int i=0;i<n;i++)
         {
-            for(int j=0; j<n; j++)
+            for(int j=0;j<n;j++)
+                scanf("%d",&a[i][j]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            memset(sum,0,sizeof(sum));
+            for(int j=i;j<n;j++)
             {
-                for(int k=i; k<n; k++)
+                int s=0;
+                for(int k=0;k<n;k++)
                 {
-                    for(int l=j; l<n; l++)
-                    {
-                        sm=a[k][l];
-                        if(i>0)
-                            sm-=a[i-1][l];
-                        if(j>0)
-                            sm-=a[k][j-1];
-                        if(i>0 && j>0)
-                            sm+=a[i-1][j-1];
-                        mx=max(mx,sm);
-                    }
+                    sum[k]+=a[j][k];
+                    s+=sum[k];
+                    if(s<sum[k])
+                        s=sum[k];
+                    mx=max(s,mx);
                 }
             }
         }
